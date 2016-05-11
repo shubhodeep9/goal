2b00a2fea5be4e8db0c2245d96ac41aa1fcfee79
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-05-11 22:01:38 +0530

    Terminal text

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var goal string = `
+ GGGGGGG    OOOOOOO                LL
+GG         OO     OO               LL
+GG   GGGG  OO     OO      AA       LL
+GG     GG  OO     OO     A  A      LL
+GG     GG  OO     OO    AAAAAA     LL
+ GGGGGGG    OOOOOOO    AA    AA    LLLLLLL
+	`
+	fmt.Println(goal)
+}
