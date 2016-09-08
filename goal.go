c0ae036b09cb33c840e45b0e5beb0d6680ce69ad
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-09-08 20:33:58 +0530

    Status courses and heading

--- goal.go
+++ goal.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
+	//"fmt"
+	"github.com/gizak/termui"
 	"github.com/urfave/cli"
+	"os"
 )
 
 func main() {
@@ -22,7 +22,43 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println(c.String("lang"))
+		err := termui.Init()
+		if err != nil {
+			return err
+		}
+		defer termui.Close()
+
+		heading := termui.NewPar("GO and Learn!")
+		heading.Height = 2
+		heading.Width = 20
+		heading.Border = false
+
+		g := termui.NewGauge()
+		g.Percent = 50
+		g.Width = 100
+		g.Height = 3
+		g.Label = ""
+		g.Y = 3
+		g.BorderLabel = "GOaL Status 50%"
+
+		courses := []string{
+			"[0]Hello World!",
+			"[1]2 + 2 = 4",
+		}
+		ls := termui.NewList()
+		ls.Items = courses
+		ls.ItemFgColor = termui.ColorYellow
+		ls.BorderLabel = "GOaL Courses"
+		ls.Height = 12
+		ls.Width = 25
+		ls.Y = 6
+		ls.X = 4
+
+		termui.Render(heading, g, ls)
+		termui.Handle("/sys/kbd/q", func(termui.Event) {
+			termui.StopLoop()
+		})
+		termui.Loop()
 		return nil
 	}
 
