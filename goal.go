c21d3ba77f651ced651cefefc6b567c019fe5cd8
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-09-08 17:08:12 +0530

    CLI Update

--- goal.go
+++ goal.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/codegangsta/cli"
+	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "goal"
-	app.Usage = "fight the loneliness!"
+	app.Name = "GOaL"
+	app.Usage = "Go and Learn, A terminal based go learning tool"
+	app.Version = "0.1.0"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "check",
+			Value: "main.go",
+			Usage: "Give the file name",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello friend!")
+		fmt.Println(c.String("lang"))
 		return nil
 	}
 
