855479018a66c5ca31259de6ea0b54f01201766d
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-11-02 00:01:22 +0530

    Make directory if not exists

--- exercises/main.go
+++ exercises/main.go
@@ -1,7 +1,6 @@
 package exercises
 
 import (
-	"fmt"
 	"github.com/shubhodeep9/goal/exercises/helloworld"
 	"os"
 	"os/user"
@@ -16,21 +15,25 @@ func ExerciseGo(index int) bool {
 	return storeCurrent(index)
 }
 
-func storeCurrent(val int) error {
+func storeCurrent(val int) bool {
 	usr, err := user.Current()
 	if err != nil {
-		return err
+		return false
 	}
-	file, err := os.OpenFile(usr.HomeDir+"/.config/goal/current.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	path := usr.HomeDir + "/.config/goal"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, os.ModePerm)
+	}
+	file, err := os.OpenFile(path+"/current.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
-		return err
+		return false
 	}
 	defer file.Close()
 	if _, err = file.Write([]byte(strconv.Itoa(val))); err != nil {
-		return err
+		return false
 	}
 	file.Sync()
-	return nil
+	return true
 }
 
 func ExerciseChecker() int {
